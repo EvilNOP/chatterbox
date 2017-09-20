aec88be48979025bf0cb65ba27c3d10a9afd2038
Matthew <evilnops@gmail.com>
2017-09-20 17:34:49 +0800

    Fixed typos

--- app/scripts/src/dom.js
+++ app/scripts/src/dom.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 
 class ChatForm {
   constructor(formSel, inputSel) {
-    this.$form = formSel;
-    this.$inputSel = inputSel;
+    this.$form = $(formSel);
+    this.$input = $(inputSel);
   }
 }
