58da9558657cbfffff341218424db7eb6dabd0d3
Matthew <evilnops@gmail.com>
2017-09-20 19:34:50 +0800

    Added `init` method

--- app/scripts/src/dom.js
+++ app/scripts/src/dom.js
@@ -5,4 +5,17 @@ class ChatForm {
     this.$form = $(formSel);
     this.$input = $(inputSel);
   }
+  
+  init(submitCallback) {
+    this.$form.submit((event) => {
+      event.preventDefault();
+      
+      let val = this.$input.val();
+      
+      submitCallback(val);
+      this.$input.val('');
+    });
+    
+    this.$form.find('button').on('click', () => this.$form.submit());
+  }
 }
